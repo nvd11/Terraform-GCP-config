288fe364733aa59fcb437c514644124a38caac33
pt0a25tujmvs <nvd11@163.com>
2023-07-22 01:33:19 +0800

    #9

--- bigquery/bq/main.tf
+++ bigquery/bq/main.tf
@@ -1,11 +1,1 @@
-data "local_file" "datasets" {
-  filename = "./datasets.tf"
-}
-
-# for tables and datasets
-module "datasets" {
-  source     = "./datasets.tf"
-  project_id = var.project_id
-  region_id = var.region_id
-}
 
