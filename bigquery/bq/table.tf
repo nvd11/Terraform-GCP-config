11ef74b3bbe6b4e6194872706f3473c7323f14bb
pt0a25tujmvs <nvd11@163.com>
2023-07-31 04:01:52 +0800

    #11

--- bigquery/bq/table.tf
+++ bigquery/bq/table.tf
@@ -1,35 +1,20 @@
+variable "table_schema" {
+  default = [
+    {
+      name = "staffid"
+      type = "STRING"
+    },
+    {
+      name = "name"
+      type = "STRING"
+    }
+  ]
+}
+
 resource "google_bigquery_table" "table1" {
   dataset_id = google_bigquery_dataset.dataset1.dataset_id
   table_id   = "table1"
   project    = var.project_id
-  schema     = jsondecode(jsonencode({
-    fields = [
-      {
-        name = "staffid"
-        type = "STRING"
-      },
-      {
-        name = "name"
-        type = "STRING"
-      }
-    ]
-  }))
+  schema     = jsonencode({ fields = var.table_schema })
 }
 
-resource "google_bigquery_table" "table2" {
-  dataset_id = google_bigquery_dataset.dataset1.dataset_id
-  table_id   = "table2"
-  project    = var.project_id
-  schema     = jsondecode(jsonencode({
-    fields = [
-      {
-        name = "staffid"
-        type = "STRING"
-      },
-      {
-        name = "name"
-        type = "STRING"
-      }
-    ]
-  }))
-}
