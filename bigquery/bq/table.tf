b75e106220486e7018cb11c614a561ad9c85c1c0
nvd11 <nvd11@163.com>
2023-07-31 20:39:05 +0800

    #11

--- bigquery/bq/table.tf
+++ bigquery/bq/table.tf
@@ -1,20 +1,31 @@
-variable "table_schema" {
-  default = [
-    {
-      name = "staffid"
-      type = "STRING"
-    },
-    {
-      name = "name"
-      type = "STRING"
-    }
-  ]
-}
-
 resource "google_bigquery_table" "table1" {
   dataset_id = google_bigquery_dataset.dataset1.dataset_id
   table_id   = "table1"
   project    = var.project_id
-  schema     = jsonencode({ fields = var.table_schema })
-}
 
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "staffid",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "The staff id"
+  },
+  {
+    "name": "name",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "the staff name"
+  }
+]
+EOF
+
+}
