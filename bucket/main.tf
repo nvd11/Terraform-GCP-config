124dc55b24f44664222138fce6473ee8518283ec
nvd11 <nvd11@163.com>
2023-09-11 01:05:32 +0800

    addded dataflow bucket to store data temp files

--- bucket/main.tf
+++ bucket/main.tf
@@ -4,6 +4,12 @@ resource "google_storage_bucket" "bucket-jason-hsbc" {
   location = var.region_id
 }
 
+resource "google_storage_bucket" "bucket-jason-hsbc-dataflow" {
+  name     = "jason-hsbc-dataflow"
+  project  = var.project_id
+  location = var.region_id
+}
+
 output "bucket-jason-hsbc-name" {
   value = google_storage_bucket.bucket-jason-hsbc.name
 }
