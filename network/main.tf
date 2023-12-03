7b90170b8cf68bdaf40c4989c9cd9dc05d0f7c8a
nvd11 <nvd11@163.com>
2023-12-04 01:26:59 +0800

    #22 test

--- network/main.tf
+++ network/main.tf
@@ -8,8 +8,6 @@ resource "google_compute_network" "tf-vpc" {
 
 # create a subnet
 resource "google_compute_subnetwork" "tf-subnet" {
-  provider = google-beta
-
   name          = "tf-subnet"
   ip_cidr_range = "192.168.0.0/24"
   region        = var.region_id
