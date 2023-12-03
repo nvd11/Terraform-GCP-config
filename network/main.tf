95a198e9a20705469f9ba4e32b435145e362d788
nvd11 <nvd11@163.com>
2023-12-04 03:18:49 +0800

    #22 test

--- network/main.tf
+++ network/main.tf
@@ -51,7 +51,7 @@ resource "google_compute_router" "tf-router" {
 
 resource "google_compute_router_nat" "tf-cloud-nat" {
   name                               = "tf-cloud-nat"
-  router                             = google_compute_router.router.name
+  router                             = google_compute_router.tf-router.name
   region                             = var.region_id
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
