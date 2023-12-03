799cb4b790b4438f098465afe4e0ad6b12bf0aa6
nvd11 <nvd11@163.com>
2023-12-04 01:38:55 +0800

    #22 test

--- network/main.tf
+++ network/main.tf
@@ -15,3 +15,21 @@ resource "google_compute_subnetwork" "tf-subnet" {
   role          = "ACTIVE"
   network       = google_compute_network.tf-vpc.id
 }
+
+# Create firewall rules
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall
+resource "google_compute_firewall" "tf-firewall" {
+  name    = "tf-firewall"
+  network = google_compute_network.tf-vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080", "80", "443", "22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # allow any external part access
+
+  # target_tags - (Optional) A list of instance tags indicating sets of instances located in the network that may make network connections as specified in allowed[]. 
+  # If no targetTags are specified, the firewall rule applies to all instances on the specified network.
+  # target_tags   = [google_compute_subnetwork.tf-subnet.name] 
+}
