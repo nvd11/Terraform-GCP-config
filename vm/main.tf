c4879e440456b35826ffce96685414b310e69794
nvd11 <nvd11@163.com>
2023-12-03 20:39:41 +0800

    #20  created 2 more k8s nodes

--- vm/main.tf
+++ vm/main.tf
@@ -4,7 +4,78 @@ resource "google_compute_instance" "k8s-master" {
   zone = var.zone_id
   
 
-  machine_type = "n2d-highmem-2"
+  machine_type = "n2d-highmem-2" # 2cpu 16GB
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size  = 20
+    }
+  }
+  
+  network_interface {
+    network = "default-vpc"
+    subnetwork = "subnet-west2"
+  }
+
+  service_account {
+    email  = "vm-common@jason-hsbc.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#provisioning_model
+  # to reduce cost
+  scheduling { 
+    automatic_restart = false # Scheduling must have preemptible be false when AutomaticRestart is true.
+    provisioning_model = "SPOT"
+    preemptible         = true
+  }
+
+}
+
+resource "google_compute_instance" "k8s-node1" {
+  name         = "k8s-node1"
+  project  = var.project_id
+  zone = var.zone_id
+  
+
+  machine_type = "n2d-highmem-4" # 4cpu 32GB
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size  = 20
+    }
+  }
+  
+  network_interface {
+    network = "default-vpc"
+    subnetwork = "subnet-west2"
+  }
+
+  service_account {
+    email  = "vm-common@jason-hsbc.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#provisioning_model
+  # to reduce cost
+  scheduling { 
+    automatic_restart = false # Scheduling must have preemptible be false when AutomaticRestart is true.
+    provisioning_model = "SPOT"
+    preemptible         = true
+  }
+
+}
+
+
+resource "google_compute_instance" "k8s-node2" {
+  name         = "k8s-node2"
+  project  = var.project_id
+  zone = var.zone_id
+  
+
+    machine_type = "n2d-highmem-4" # 4cpu 32GB
   
   boot_disk {
     initialize_params {
