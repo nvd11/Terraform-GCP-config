69654e1ddf78ef8454b9a578c3dec30e9aac1ea5
nvd11 <nvd11@163.com>
2023-12-22 04:25:46 +0800

    #1  created ds2

---
+++ bigquery/bq_ds2/table.tf
@@ -0,0 +1,35 @@
+resource "google_bigquery_table" "sales_details" {
+  dataset_id = google_bigquery_dataset.dataset1.dataset_id
+  table_id   = "sales_details"
+  project    = var.project_id
+
+  schema = <<EOF
+[
+    {
+        "name": "Order_ID",
+        "type": "STRING"
+    },
+    {
+        "name": "Product",
+        "type": "STRING"
+    },
+    {
+        "name": "Quantity_Ordered",
+        "type": "INTEGER"
+    },
+    {
+        "name": "Price_Each",
+        "type": "FLOAT"
+    },
+    {
+        "name": "Order_Date",
+        "type": "STRING"
+    },
+    {
+        "name": "Purchase_Address",
+        "type": "STRING"
+    }
+]
+EOF
+
+}
