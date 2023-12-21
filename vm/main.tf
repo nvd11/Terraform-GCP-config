aabd7aa7501ee7f47b8e94728992cce0098a918a
nvd11 <nvd11@163.com>
2023-12-21 23:44:20 +0800

    #34 testing

--- vm/main.tf
+++ vm/main.tf
@@ -318,5 +318,5 @@ resource "google_compute_instance_from_template" "tf-vpc1-subnet0-vm20" {
   allow_stopping_for_update = true
 
   # from a instance template
-  source_instance_template = "e2-small-tomcat"
+  source_instance_template = "https://www.googleapis.com/compute/v1/projects/jason-hsbc/global/instanceTemplates/e2-small-tomcat"
 }
