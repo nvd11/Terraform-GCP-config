7fab1f24231da8388be3222ab3da1d10e7363daa
nvd11 <nvd11@163.com>
2023-12-22 01:23:39 +0800

    #34 test

--- vm/vm_from_template.tf
+++ vm/vm_from_template.tf
@@ -18,3 +18,16 @@ resource "google_compute_instance_from_template" "tf-vpc0-subnet0-vm21" {
   # from a instance template
   source_instance_template = google_compute_instance_template.vm-template-vpc0-subnet0-e2-small-tomcat.self_link_unique
 }
+
+resource "google_compute_instance_from_template" "tf-vpc0-subnet0-vm22" {
+  name         = "tf-vpc0-subnet0-vm22"
+  project      = var.project_id
+  zone         = var.zone_id
+
+  network_interface {
+    network =  "tf-vpc0"
+    subnetwork =  "tf-vpc0-subnet1"
+  }
+  # from a instance template
+  source_instance_template = google_compute_instance_template.vm-template-vpc0-subnet0-e2-small-tomcat.self_link_unique
+}
