a729209c62a1fecdb1d8d8ae6b1a96d225daade7
nvd11 <nvd11@163.com>
2023-12-22 23:00:45 +0800

    #1 fixed partition field type

--- bigquery/bq_ds2/table.tf
+++ bigquery/bq_ds2/table.tf
@@ -34,7 +34,8 @@ EOF
 
 }
 
-
+# The field for partition must be one of the following types: DATE, Timestamp, or Integer. 
+# otherwise :  Error: googleapi: Error 400: The field specified for time partitioning can only be of type TIMESTAMP, DATE or DATETIME. The type found is: STRING., invalid
 resource "google_bigquery_table" "sales_details_p" {
   dataset_id = google_bigquery_dataset.dataset1.dataset_id
   table_id   = "sales_details"
@@ -60,7 +61,7 @@ resource "google_bigquery_table" "sales_details_p" {
     },
     {
         "name": "Order_Date",
-        "type": "STRING"
+        "type": "DATE"
     },
     {
         "name": "Purchase_Address",
