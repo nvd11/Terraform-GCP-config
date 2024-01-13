dceecac1c3b055791275bbeb3ad0a997ad4b5a04
nvd11 <nvd11@163.com>
2024-01-14 03:32:12 +0800

    #44 created 2 buckets

--- bucket/main.tf
+++ bucket/main.tf
@@ -10,6 +10,20 @@ resource "google_storage_bucket" "bucket-jason-hsbc-dataflow" {
   location = var.region_id
 }
 
+resource "google_storage_bucket" "bucket-jason-hsbc-src" {
+  name     = "jason-hsbc-src"
+  project  = var.project_id
+  location = var.region_id
+}
+
+
+resource "google_storage_bucket" "bucket-jason-hsbc-des" {
+  name     = "jason-hsbc-des"
+  project  = var.project_id
+  location = var.region_id
+}
+
+
 output "bucket-jason-hsbc-name" {
   value = google_storage_bucket.bucket-jason-hsbc.name
 }
