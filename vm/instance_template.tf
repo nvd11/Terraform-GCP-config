4c2db10b3ef8b169dbf44d0807035834c6487b23
nvd11 <nvd11@163.com>
2024-02-24 03:30:08 +0800

    added templates for k8s master and nodes

--- vm/instance_template.tf
+++ vm/instance_template.tf
@@ -32,3 +32,69 @@ resource "google_compute_instance_template" "vm-template-vpc0-subnet0-e2-small-t
 }
 
 
+resource "google_compute_instance_template" "vm-template-k8s-nodes" {
+  name         = "vm-template-k8s-nodes"
+  machine_type = "n2d-highmem-4" # 4cpu 32GB
+
+  disk {
+    source_image = "https://compute.googleapis.com/compute/v1/projects/jason-hsbc/global/images/debian-11-gateman-root-k8snode"
+    auto_delete  = true
+    disk_size_gb = 20
+    boot         = true
+  }
+
+  network_interface {
+    network =  var.vpc0
+    subnetwork =  var.vpc0_subnet0    
+  }
+
+ service_account {
+    email  = "vm-common@jason-hsbc.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#provisioning_model
+  # to reduce cost
+  scheduling { 
+    automatic_restart = false # Scheduling must have preemptible be false when AutomaticRestart is true.
+    provisioning_model = "SPOT"
+    preemptible         = true
+    instance_termination_action = "STOP"
+  }
+
+  #can_ip_forward = false
+}
+
+
+resource "google_compute_instance_template" "vm-template-k8s-master" {
+  name         = "vm-template-k8s-master"
+  machine_type = "n2d-highmem-4" # 4cpu 32GB
+
+  disk {
+    source_image = "https://compute.googleapis.com/compute/v1/projects/jason-hsbc/global/images/debian-11-gateman-root-k8smaster"
+    auto_delete  = true
+    disk_size_gb = 20
+    boot         = true
+  }
+
+  network_interface {
+    network =  var.vpc0
+    subnetwork =  var.vpc0_subnet0    
+  }
+
+ service_account {
+    email  = "vm-common@jason-hsbc.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#provisioning_model
+  # to reduce cost
+  scheduling { 
+    automatic_restart = false # Scheduling must have preemptible be false when AutomaticRestart is true.
+    provisioning_model = "SPOT"
+    preemptible         = true
+    instance_termination_action = "STOP"
+  }
+
+  #can_ip_forward = false
+}
--- vm/k8s-vms.tf
+++ vm/k8s-vms.tf
@@ -70,39 +70,23 @@ resource "google_compute_instance" "k8s-node0" {
 
 }
 
-
-resource "google_compute_instance" "k8s-node1" {
+resource "google_compute_instance_from_template" "k8s-node1" {
   name         = "k8s-node1"
-  project  = var.project_id
-  zone = var.zone_id
-  
+  project      = var.project_id
+  zone         = var.zone_id
   allow_stopping_for_update = true
-  machine_type = "n2d-highmem-4" # 4cpu 32GB
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      size  = 20
-    }
-  }
-  
-  network_interface {
-    network =  var.vpc0
-    subnetwork =  var.vpc0_subnet0
-  }
 
-  service_account {
-    email  = "vm-common@jason-hsbc.iam.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
+  # from a instance template
+  source_instance_template = "https://www.googleapis.com/compute/v1/projects/jason-hsbc/global/instanceTemplates/vm-template-k8s-nodes"
+}
 
-  # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#provisioning_model
-  # to reduce cost
-  scheduling { 
-    automatic_restart = false # Scheduling must have preemptible be false when AutomaticRestart is true.
-    provisioning_model = "SPOT"
-    preemptible         = true
-    instance_termination_action = "STOP"
-  }
 
+resource "google_compute_instance_from_template" "k8s-node2" {
+  name         = "k8s-node2"
+  project      = var.project_id
+  zone         = var.zone_id
+  allow_stopping_for_update = true
+
+  # from a instance template
+  source_instance_template = "https://www.googleapis.com/compute/v1/projects/jason-hsbc/global/instanceTemplates/vm-template-k8s-nodes"
 }
