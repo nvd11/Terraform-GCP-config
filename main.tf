db3d00e4346e59354cb962e2fbbc94edf0675789
nvd11 <nvd11@163.com>
2024-06-09 02:18:06 +0800

    added cloudbuid trigger

--- main.tf
+++ main.tf
@@ -73,3 +73,10 @@ module "service_account" {
   region_id = var.region_id
   zone_id = var.zone_id
 }
+
+module "cloudbuild" {
+  source     = "./cloudbuild"
+  project_id = var.project_id
+  region_id = var.region_id
+  zone_id = var.zone_id
+}
