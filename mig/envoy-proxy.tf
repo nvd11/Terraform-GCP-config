8c245dddc72016094d7d4be50a4b8ba69200ac15
nvd11 <nvd11@163.com>
2025-10-11 15:53:48 +0800

    feat: Update envoy-proxy to use MIG module v1.0.0 and enable Spot VMs

---
+++ mig/envoy-proxy.tf
@@ -0,0 +1,19 @@
+# use mig module# This data source looks up the existing 'vm-common' service account.
+# It does NOT create a new one. This is the standard way to reference
+# resources that are managed outside of this Terraform configuration.
+data "google_service_account" "vm_common" {
+  account_id = "vm-common"
+}
+
+module "my_new_mig" {
+  source                = "git@github.com:nvd11/terraform-mig.git?ref=1.0.0"
+  
+  name                  = "my-envoy"
+  zone                  = var.zone_id
+  machine_type          = "e2-medium"
+  source_image          = "projects/jason-hsbc/global/images/packer-gce-envoy6"
+  subnetwork            = "tf-vpc0-subnet0"
+  service_account_email = data.google_service_account.vm_common.email
+  target_size           = 1
+  spot                  = true
+}
