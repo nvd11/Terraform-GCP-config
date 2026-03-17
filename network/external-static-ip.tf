0b61df3058c2b9ebe1e66f817713ee44a93ed616
nvd11 <nvd11@163.com>
2026-03-18 01:40:58 +0800

    feat: add openclaw spot vm and placeholder static ip
    - Provisioned a new spot compute instance `tf-vpc0-subnet0-openclaw` (n2d-standard-4) in vpc0-subnet0. Configured as preemptible with a STOP termination action to reduce costs.
    - Added a placeholder external static IP `static-ip-2` with count set to 0.

--- network/external-static-ip.tf
+++ network/external-static-ip.tf
@@ -4,3 +4,9 @@ resource "google_compute_address" "static-ip-1" {
   address_type = "EXTERNAL"
 }
 
+resource "google_compute_address" "static-ip-2" {
+  count = 0
+  name = "static-ip-2"
+  address_type = "EXTERNAL"
+}
+
