ca71e3bc09b6dcdd8998287a69198480014a885f
nvd11 <nvd11@163.com>
2026-03-21 03:39:57 +0800

    feat(network): provision external static IP for envoy proxy
    Rename the inactive static-ip-2 resource to static-ip-envoy-proxy and update the count from 0 to 1. This enables the provisioning of a dedicated external static IP address to be used by the Envoy proxy.

--- network/external-static-ip.tf
+++ network/external-static-ip.tf
@@ -4,9 +4,9 @@ resource "google_compute_address" "static-ip-1" {
   address_type = "EXTERNAL"
 }
 
-resource "google_compute_address" "static-ip-2" {
-  count = 0
-  name = "static-ip-2"
+resource "google_compute_address" "static-ip-envoy-proxy" {
+  count = 1
+  name = "static-ip-envoy-proxy"
   address_type = "EXTERNAL"
 }
 
